887e9e390baccb0e2a27f1c535b077799475b7aa
Gus Castles <guscastles@gmail.com>
2018-11-24 20:01:44 +1100

    Updated unittest and cmake configuration

--- test/unittest.c
+++ test/unittest.c
@@ -6,38 +6,41 @@
 #define SIZE(v) (sizeof(v) / sizeof(v[0]))
 
 typedef struct unittest {
-   char name[40];
-   void (*func)(void);
+    char name[40];
+    void (*func)(void);
 } Test;
 
 void add_tests(CU_pSuite suite, Test *tests, int size, int next) {
-   if (next < size) {
-      CU_add_test(suite, tests[next].name, tests[next].func);
-      add_tests(suite, tests, size, next + 1);
-   }
+    if (next < size) {
+       CU_add_test(suite, tests[next].name, tests[next].func);
+       add_tests(suite, tests, size, next + 1);
+    }
 }
 
-void runme(Test *tests, int size){
-   CU_initialize_registry();
-   CU_pSuite suite = CU_add_suite("hanoi_test", 0, 0);
-   add_tests(suite, tests, size, 0);
-   CU_basic_set_mode(CU_BRM_VERBOSE);
-   CU_basic_run_tests();
-   CU_cleanup_registry();
+int runme(Test *tests, int size) {
+    CU_initialize_registry();
+    CU_pSuite suite = CU_add_suite("hanoi_test", 0, 0);
+    add_tests(suite, tests, size, 0);
+    CU_basic_set_mode(CU_BRM_VERBOSE);
+    CU_basic_run_tests();
+    unsigned int errors = CU_get_number_of_tests_failed();
+    CU_cleanup_registry();
+    return errors;
 }
 
-void gather_tests(void (*run)()) {
-   int size = SIZE(names);
-   Test tests[size];
-   for (int i = 0; i < size; ++i) {
-      strcpy(tests[i].name, names[i]);
-      tests[i].func = functions[i];
-   }
-   run(tests, SIZE(tests));
+int gather_tests(int (*run)()) {
+    int size = SIZE(names);
+    Test tests[size];
+    for (int i = 0; i < size; ++i) {
+        strcpy(tests[i].name, names[i]);
+        tests[i].func = functions[i];
+    }
+    return run(tests, SIZE(tests));
 }
 
 int main(void) {
-   gather_tests(runme);
-   return 0;
+    unsigned int errors = gather_tests(runme);
+    printf("\n#### Errors: %d ####\n", errors);
+    return errors;
 }
 
